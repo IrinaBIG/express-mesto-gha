99a27ae1cf35c67e4a3a0c3935a9632e8feb0a47
Irina Berestova <berestovaecdep@mail.ru>
2022-10-18 21:31:41 +0700

    сделать валидацию

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -5,8 +5,7 @@ module.exports.celebrateSignUp = celebrate({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string(),
-    email: Joi.string().required(),
-    // email: Joi.string().required().email(),
+    email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 });
@@ -25,9 +24,22 @@ module.exports.celebrateUpdateUser = celebrate({
   }),
 });
 
-// module.exports.celebrateUserAvatarByID = celebrate({
-//   body: Joi.object().keys({
-//     email: Joi.string().required().email(),
-//     password: Joi.string().required(),
-//   }),
-// });
+module.exports.celebrateUserAvatarByID = celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string(),
+  }),
+});
+
+module.exports.celebrateCreateCard = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    link: Joi.string().required(),
+  }),
+});
+
+module.exports.celebrateDeleteAndLikesCard = celebrate({
+  // валидируем параметры
+  params: Joi.object().keys({
+    id: Joi.string().alphanum().length(24), // длина id = 24
+  }),
+});
--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,13 @@ const router = require('express').Router();
 const {
   getUsers, getUserId, updateUserProfileByID, updateUserAvatarByID, getUsersMe,
 } = require('../controllers/users');
-const { celebrateUpdateUser } = require('../middlewares/validation');
+
+const { celebrateUpdateUser, celebrateUserAvatarByID } = require('../middlewares/validation');
 
 router.get('/', getUsers);
 router.get('/me', getUsersMe);
 router.patch('/me', celebrateUpdateUser, updateUserProfileByID);
-router.patch('/me/avatar', updateUserAvatarByID);
+router.patch('/me/avatar', celebrateUserAvatarByID, updateUserAvatarByID);
 router.get('/:_id', getUserId);
 
 module.exports = router;
