4eb5aa5b5a7fbbb05900ab4a68ab017a7bba6e73
Irina Berestova <berestovaecdep@mail.ru>
2022-10-19 21:40:47 +0700

    сделать централизованную обработку ошибок, кроме, создания пользователя

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
 const auth = require('./middlewares/auth');
 const { login, createUser } = require('./controllers/users');
 const { celebrateSignUp, celebrateSignIn } = require('./middlewares/validation');
+const handlerErrors = require('./middlewares/handlerErrors');
 
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
@@ -21,6 +23,10 @@ app.use(auth);
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 app.use('*', require('./routes/notFound'));
+
+// обработчики ошибок
+app.use(errors()); // обработчик ошибок celebrate
+app.use(handlerErrors); // централизованнный обработчик
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
