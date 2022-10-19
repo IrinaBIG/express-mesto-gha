9434c6274189798aa6ab67569d69f58409454eff
Irina Berestova <berestovaecdep@mail.ru>
2022-10-19 21:48:13 +0700

    убрать ошибку теста по валидации через joi пользователя с некорректным id

--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,12 @@ const {
   getUsers, getUserId, updateUserProfileByID, updateUserAvatarByID, getUsersMe,
 } = require('../controllers/users');
 
-const { celebrateUpdateUser, celebrateUserAvatarByID } = require('../middlewares/validation');
+const { celebrateUpdateUser, celebrateUserAvatarByID, celebrateDeleteAndLikesCard } = require('../middlewares/validation');
 
 router.get('/', getUsers);
 router.get('/me', getUsersMe);
 router.patch('/me', celebrateUpdateUser, updateUserProfileByID);
 router.patch('/me/avatar', celebrateUserAvatarByID, updateUserAvatarByID);
-router.get('/:_id', getUserId);
+router.get('/:_id', celebrateDeleteAndLikesCard, getUserId);
 
 module.exports = router;
