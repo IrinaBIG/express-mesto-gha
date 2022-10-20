e2fd9a62fe8c181fa370128ed3bbbde9ffca33a3
Irina Berestova <berestovaecdep@mail.ru>
2022-10-20 20:42:21 +0700

    сделать regex

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -2,14 +2,15 @@ const { celebrate, Joi } = require('celebrate');
 
 // const regexUrl = /\bhttps?:\/\/([a-zA-Z0-9]{1,}-]+.]|[$-_@.&+!*,]|(?:%[0-9a-fA-F][0-9a-fA-F]))+/;
 // const regexUrl = /https?:\/\/[a-zA-Z0-9\-.]+\.[a-zA-Z]{2,3}(\/\S*)?/;
-const regexUrl = /\bhttps?:\/\/(?:[a-zA-Z0-9]|[$-_@.&+]|[!*,]|(?:%[0-9a-fA-F][0-9a-fA-F]))+/;
+// const regexUrl = /\bhttps?:\/\/(?:[a-zA-Z0-9]|[$-_@.&+]|[!*,]|(?:%[0-9a-fA-F][0-9a-fA-F]))+/;
 // const regexUrl = /http[s]?:\/\/(?:[0-9]|[a-zA-Z]|[$-_@.&+]|[!*,]|(?:%[0-9a-fA-F][0-9a-fA-F]))+/;
+const regexUrl = /^(https?):\/\/(\w+|\W+)+(\.)|(\w+|\W+)\.ru#$/;
 
 module.exports.celebrateSignUp = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().required().pattern(regexUrl),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
